4b59ca9d5a64e656f29d4f6617caf6e02cedbcb9
ioviic <>
2020-04-18 21:15:09 +0300

    add correct configuration

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export default {
   s3: {
-    REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-    BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    REGION: "eu-central-1",
+    BUCKET: "serverless-vb-bucket"
   },
   apiGateway: {
-    REGION: "YOUR_API_GATEWAY_REGION",
-    URL: "YOUR_API_GATEWAY_URL"
+    REGION: "eu-central-1",
+    URL: "https://cxwhsvcst7.execute-api.eu-central-1.amazonaws.com/prod"
   },
   cognito: {
-    REGION: "YOUR_COGNITO_REGION",
-    USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-    APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-    IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_beIwlB52V",
+    APP_CLIENT_ID: "2vevr3u2so84j2junq469l4p4c",
+    IDENTITY_POOL_ID: "eu-central-1:a251ea01-12ea-4347-b2ca-43d11262cc8e"
   }
 };
